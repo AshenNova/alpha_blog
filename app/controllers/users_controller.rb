1d717dbbe2942986a82a4a0d4fc3e9185a102c15
AshenNova <Kennerve14@gmail.com>
2021-02-24 15:29:27 +0800

    Add users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,11 @@ class UsersController < ApplicationController
 
   def index
   end
+
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
   
   def new
     @user = User.new
