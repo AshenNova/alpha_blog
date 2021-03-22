99c8ff0fa1b094b6e5bec9a7eeee05029769170b
AshenNova <Kennerve14@gmail.com>
2021-03-22 13:32:34 +0800

    Commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 class UsersController < ApplicationController
+  
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end
 
   def show
-    @user = User.find(params[:id])
+
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
   
@@ -14,11 +18,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+
   end
 
   def update
-    @user = User.find(params[:id])
+
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated"
       redirect_to user_path(@user)
@@ -27,9 +31,22 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user.destroy
+    if @user == current_user
+      session[:user_id] = nil
+      flash[:notice] = "Account and all associated articles successfully deleted"
+      redirect_to articles_path
+    else
+      flash[:notice] = "Account and all associated articles successfully deleted"
+      redirect_to users_path
+    end
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
       redirect_to articles_path
     else
@@ -38,7 +55,18 @@ class UsersController < ApplicationController
   end
 
   private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
     def user_params
       params.require(:user).permit(:username, :email, :password)
+    end
+
+    def require_same_user
+      if current_user != @user && !current_user.admin?
+        flash[:alert] = "You can only edit or delete your own account"
+        redirect_to @user
+      end
     end
 end
